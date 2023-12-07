3cae2acab07ed40e9b412a74ac587489ab0e912f
Brady Edwards <brady.edwards16@gmail.com>
2023-12-06 23:49:19 -0500

    Day3 Part 2

--- src/puzzles/day3.rs
+++ src/puzzles/day3.rs
@@ -52,7 +52,6 @@ pub fn solve1(data: &String) {
     for r in 0..row {
         if engine_part_present {
             result += curr;
-            engine_part_present = false;
         }
         curr = 0;
         engine_part_present = false;
@@ -71,7 +70,6 @@ pub fn solve1(data: &String) {
             } else {
                 if engine_part_present {
                     result += curr;
-                    engine_part_present = false;
                 }
                 curr = 0;
                 engine_part_present = false;
@@ -82,8 +80,73 @@ pub fn solve1(data: &String) {
     println!("Part 1 = {}", result);
 }
 
+use::std::collections::{HashMap, HashSet};
+
+pub fn gear_neighbor(r: i32, c: i32, grid: &Vec<Vec<char>>, curr_gears: &mut HashSet<(usize, usize)>) {
+    if r < 0 || c < 0 || r as usize == grid.len() 
+        || c as usize == grid.len() || grid[r as usize][c as usize] != '*' {
+            return;
+    } else {
+        curr_gears.insert((r as usize, c as usize));
+    }
+}
+
 pub fn solve2(data: &String) {
-    // grid = data.lines().collect::<Vec<_>>();
-    let result = "TBD";
-    println!("Result = {result}");
+    let grid = data.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let mut result = 0;
+
+    let mut gears: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+
+    let mut curr: i32 = 0;
+    let mut curr_gear = HashSet::new();
+
+    let row = grid.len();
+    let col = grid[0].len();
+
+    for r in 0..row {
+        if !curr_gear.is_empty() {
+            for gear in curr_gear.iter() {
+                if gears.contains_key(gear) {
+                    gears.get_mut(gear).unwrap().push(curr);
+                } else {
+                    gears.insert(*gear, vec![curr]);
+                }
+            }
+        }
+        curr = 0;
+        curr_gear.clear();
+
+        for c in 0..col {
+            if grid[r][c].is_numeric() {
+                curr = 10 * curr + grid[r][c].to_digit(10).unwrap() as i32;
+                gear_neighbor(r as i32 + 1, c as i32 + 1, &grid, &mut curr_gear);
+                gear_neighbor(r as i32 + 1, c as i32, &grid, &mut curr_gear);
+                gear_neighbor(r as i32 + 1, c as i32 - 1, &grid, &mut curr_gear);
+                gear_neighbor(r as i32, c as i32 + 1, &grid, &mut curr_gear);
+                gear_neighbor(r as i32, c as i32 - 1, &grid, &mut curr_gear);
+                gear_neighbor(r as i32 - 1, c as i32 + 1, &grid, &mut curr_gear);
+                gear_neighbor(r as i32 - 1, c as i32, &grid, &mut curr_gear);
+                gear_neighbor(r as i32 - 1, c as i32 - 1, &grid, &mut curr_gear);
+            } else {
+                if !curr_gear.is_empty() {
+                    for gear in curr_gear.iter() {
+                        if gears.contains_key(gear) {
+                            gears.get_mut(gear).unwrap().push(curr);
+                        } else {
+                            gears.insert(*gear, vec![curr]);
+                        }
+                    }
+                }
+                curr = 0;
+                curr_gear.clear();
+            }
+        }
+    }
+
+    for (_, ratios) in gears {
+        if ratios.len() == 2 {
+            result += ratios[0] * ratios[1];
+        }
+    }
+    println!("Part 2 = {result}");
 }
