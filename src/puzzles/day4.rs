814b58cd631c4622b4f0148d868f976b1e80e5ac
Brady Edwards <brady.edwards16@gmail.com>
2023-12-08 02:12:51 -0500

    Day4 part 2

--- src/puzzles/day4.rs
+++ src/puzzles/day4.rs
@@ -20,15 +20,38 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     ));
 }
 
-use::std::collections::{HashMap, HashSet};
+use::std::collections::HashSet;
 
 pub fn solve1(data: &String) {
-    let result = 0;
+    let mut score = 0;
 
-    println!("Part 1 = {}", result);
+    for line in data.lines() {
+        let nums: Vec<&str> = line.split('|').collect();
+        let winners: HashSet<i32> = nums[0].split_whitespace().filter(|c| c.parse::<i32>().is_ok()).map(|n| n.parse::<i32>().unwrap()).collect();
+        let my_nums: HashSet<i32> = nums[1].split_whitespace().filter(|c| c.parse::<i32>().is_ok()).map(|n| n.parse::<i32>().unwrap()).collect();
+
+        let p = winners.intersection(&my_nums).count();
+        if p == 0 {continue;}
+        score += 2i32.pow(p as u32 - 1);
+    }
+
+    println!("Part 1 = {}", score);
 }
 
 pub fn solve2(data: &String) {
-    let result = data;
-    println!("Part 2 = {result}");
+    let mut tickets = vec![1; data.lines().count()];
+
+    for (index, line) in data.lines().enumerate() {
+        let nums: Vec<&str> = line.split('|').collect();
+        let winners: HashSet<i32> = nums[0].split_whitespace().filter(|c| c.parse::<i32>().is_ok()).map(|n| n.parse::<i32>().unwrap()).collect();
+        let my_nums: HashSet<i32> = nums[1].split_whitespace().filter(|c| c.parse::<i32>().is_ok()).map(|n| n.parse::<i32>().unwrap()).collect();
+
+        let m = winners.intersection(&my_nums).count();
+
+        for i in (index+1)..(index+1+m) {
+            tickets[i] += tickets[index];
+        }
+    }
+
+    println!("Part 2 = {}", tickets.iter().sum::<i32>());
 }
