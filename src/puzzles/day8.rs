414b500bb7c8a35c94ed10bf16a0cfe2902a2e81
Brady Edwards <brady.edwards16@gmail.com>
2023-12-11 16:49:23 -0500

    Day 8 setup

---
+++ src/puzzles/day8.rs
@@ -0,0 +1,23 @@
+#[test]
+
+fn test() {
+    let data = String::from(
+""
+    );
+
+    solve1(&data);
+
+    solve2(&data);
+}
+
+pub fn solve1(data: &String) {
+    let _data = data.lines().collect::<Vec<_>>();
+    let res = 0;
+    println!("Part 1 = {}", res);
+}
+
+pub fn solve2(data: &String) {
+    let _data = data.lines().collect::<Vec<_>>();
+    let res = 0;
+    println!("Part 2 = {}", res);
+}
