f9c834cd1d231a6a4e78a8642bf3c017f3ed24b5
Brady Edwards <brady.edwards16@gmail.com>
2023-12-12 00:08:14 -0500

    Day 9 Part 2

--- src/puzzles/day9.rs
+++ src/puzzles/day9.rs
@@ -13,13 +13,44 @@ fn test() {
 }
 
 pub fn solve1(data: &String) {
-    let _data = data.lines().collect::<Vec<_>>();
+    let data = data.lines().collect::<Vec<_>>();
+    let mut res = 0;
 
-    println!("Part 1 = {}", 0);
+    for line in data {
+        let mut oasis: Vec<i32> = line.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect();
+        let mut final_sum: Vec<i32> = vec![];
+        while !oasis.iter().all(|&n| n == 0) {
+            let mut diffs: Vec<i32> = vec![];
+            for i in 0..(oasis.len()-1) {
+                diffs.push(oasis[i + 1] - oasis[i]);
+            }
+            let last = oasis.pop().unwrap();
+            final_sum.push(last);
+            oasis = diffs;
+        }
+        res += final_sum.iter().sum::<i32>();
+    }
+
+    println!("Part 1 = {}", res);
 }
 
 pub fn solve2(data: &String) {
-    let _data = data.lines().collect::<Vec<_>>();
+    let data = data.lines().collect::<Vec<_>>();
+    let mut res = 0;
 
-    println!("Part 2 = {}", 0);
+    for line in data {
+        let mut oasis: Vec<i32> = line.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect();
+        let mut final_sum: Vec<i32> = vec![];
+        while !oasis.iter().all(|&n| n == 0) {
+            let mut diffs: Vec<i32> = vec![];
+            for i in 0..(oasis.len()-1) {
+                diffs.push(oasis[i + 1] - oasis[i]);
+            }
+            let first = oasis[0];
+            final_sum.push(first);
+            oasis = diffs;
+        }
+        res += final_sum.iter().rev().fold(0, |a, &b| -(a-b));
+    }
+    println!("Part 2 = {}", res);
 }
