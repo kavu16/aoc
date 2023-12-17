475a96e37622f93f85c10e3b490a764e2954b5c3
Brady Edwards <brady.edwards16@gmail.com>
2023-12-16 23:32:39 -0500

    Day 11 Part 1 & 2

--- src/puzzles/day11.rs
+++ src/puzzles/day11.rs
@@ -40,15 +40,85 @@ pub fn solve1(data: &String) {
             }
         }
     }
-    
-    println!("Number of galaxies = {}", galaxy_locs.len());
-    println!("Part 1 = {}", 0);
+
+    fn galaxy_taxi_dist(x: (i32, i32), y: (i32, i32), empty_x: &HashSet<usize>, empty_y: &HashSet<usize>) -> i32 {
+        let x_min = std::cmp::min(x.0, y.0);
+        let x_max = std::cmp::max(x.0, y.0);
+        let y_min = std::cmp::min(x.1, y.1);
+        let y_max = std::cmp::max(x.1, y.1);
+        let mut expansion = 0;
+        for x in x_min..x_max {
+            if empty_x.contains(&(x as usize)) {
+                expansion += 1;
+            }
+        }
+        for y in y_min..y_max {
+            if empty_y.contains(&(y as usize)) {
+                expansion += 1;
+            }
+        }
+        (x_max - x_min) + (y_max - y_min) + expansion
+    }
+
+    let mut res = 0;
+
+    galaxy_locs.iter().for_each(|g| {
+        res += galaxy_locs.iter().map(|d| galaxy_taxi_dist(*g, *d, &empty_rows, &empty_cols)).sum::<i32>();
+    });
+
+    res /= 2;
+
+    println!("Part 1 = {}", res);
 }
 
 pub fn solve2(data: &String) {
-    let _data = data.lines().collect::<Vec<_>>();
+    let data = data.lines().collect::<Vec<_>>();
+    let galaxy: Vec<Vec<char>> = data.iter().map(|l| l.chars().collect::<Vec<char>>()).collect();
+    let rows = galaxy.len();
+    let cols = galaxy[0].len();
 
-    println!("Part 2 = {}", 0);
+    let mut empty_rows: HashSet<usize> = (0..rows).into_iter().collect();
+    let mut empty_cols: HashSet<usize> = (0..cols).into_iter().collect();
+    let mut galaxy_locs: Vec<(i64, i64)> = vec![];
+
+    for r in 0..rows {
+        for c in 0..cols {
+            if galaxy[r][c] == '#' {
+                empty_rows.remove(&r);
+                empty_cols.remove(&c);
+                galaxy_locs.push((r as i64, c as i64));
+            }
+        }
+    }
+
+    fn galaxy_taxi_dist(x: (i64, i64), y: (i64, i64), empty_x: &HashSet<usize>, empty_y: &HashSet<usize>) -> i64 {
+        let x_min = std::cmp::min(x.0, y.0);
+        let x_max = std::cmp::max(x.0, y.0);
+        let y_min = std::cmp::min(x.1, y.1);
+        let y_max = std::cmp::max(x.1, y.1);
+        let mut expansion = 0;
+        for x in x_min..x_max {
+            if empty_x.contains(&(x as usize)) {
+                expansion += 999_999;
+            }
+        }
+        for y in y_min..y_max {
+            if empty_y.contains(&(y as usize)) {
+                expansion += 999_999;
+            }
+        }
+        (x_max - x_min) + (y_max - y_min) + expansion
+    }
+
+    let mut res = 0;
+
+    galaxy_locs.iter().for_each(|g| {
+        res += galaxy_locs.iter().map(|d| galaxy_taxi_dist(*g, *d, &empty_rows, &empty_cols)).sum::<i64>();
+    });
+
+    res /= 2;
+
+    println!("Part 2 = {}", res);
 }
 
 //  [2 5 3 4 8]
