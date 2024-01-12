822b297082e67ac3dd245c38da065872a54a6870
Brady Edwards <brady.edwards16@gmail.com>
2024-01-12 12:02:08 -0500

    Day 21 Pt 2

--- src/puzzles/day21.rs
+++ src/puzzles/day21.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{VecDeque, HashSet};
 
 #[test]
 
@@ -19,7 +19,7 @@ fn test() {
     // S at (5, 5)
     // assert_eq!(solve1(&example1), 16);
 
-    assert_eq!(solve2(&example1), 50);
+    assert_eq!(solve2(&example1), 16);
 }
 
 pub fn solve1(data: &String) -> usize {
@@ -59,55 +59,103 @@ pub fn solve1(data: &String) -> usize {
             }
         }
     }
-    let res = garden.iter().flatten().filter(|(plot, _)| plot == &'O').count();
+    let res = garden.iter()
+                           .flatten()
+                           .filter(|(plot, _)| plot == &'O')
+                           .count();
 
-    // let res = 0;
     println!("Part 1 = {}", res);
     res
 }
 
 pub fn solve2(data: &String) -> usize {
-    let mut garden = Vec::<Vec<((char, usize),(char, usize))>>::new();
+    let mut garden = Vec::<Vec<char>>::new();
     for line in data.lines() {
-        garden.push(line.chars().map(|c| ((c,0), (c,0))).collect::<Vec<((char, usize),(char, usize))>>());
+        garden.push(
+            line.chars()
+                .collect::<Vec<char>>()
+        );
     }
-    let (row, col) = (garden.len(), garden[0].len());
-    let start = (5, 5);
-    let mut queue = VecDeque::<(usize, usize)>::new();
-    queue.push_back(start);
 
-    for _ in 0..50 {
-        for _ in 0..queue.len() {
-            let (r, c) = queue.pop_front().unwrap();
-            if garden[(row + r-1) % row][c] != (('#', 0), ('#', 0)) && !queue.contains(&((row + r-1) % row, c)) {
-                garden[(row + r-1) % row][c].1.0 = 'O';
-                garden[(row + r-1) % row][c].1.1 += 1;
-                queue.push_back(((row + r-1) % row, c));
+    let row = garden.len();
+    let col = garden[0].len();
+    assert_eq!(row, col);
+    let size = row;
+
+    let total_steps = 26_501_365;
+    let original = total_steps % (2 * size);
+
+    let reachable_plots = |x: usize| -> usize {
+        let mut plots = HashSet::<(i128, i128)>::new();
+        let mut visited = HashSet::<(i128, i128)>::new();
+        let mut queue = VecDeque::<(i128, i128, usize)>::new();
+
+        let start = ((row/2) as i128, (col/2) as i128, original + 2 * size * x);
+        println!("{start:?}");
+        queue.push_front(start); 
+        visited.insert((start.0, start.1));
+
+        while !queue.is_empty() {
+            let (r, c, step) = queue.pop_front().unwrap();
+
+            if step % 2 == 0 {
+                plots.insert((r, c));
             }
-            if garden[(row + r+1) % row][c] != (('#', 0), ('#', 0)) && !queue.contains(&((row + r+1) % row, c)) {
-                garden[(row + r+1) % row][c].1.0 = 'O';
-                garden[(row + r+1) % row][c].1.1 += 1;
-                queue.push_back(((row + r+1) % row, c));
+            if step == 0 {
+                continue;
             }
-            if garden[r][(col + c-1) % col] != (('#', 0), ('#', 0)) && !queue.contains(&(r, (col + c-1) % col)) {
-                garden[r][(col + c-1) % col].1.0 = 'O';
-                garden[r][(col + c-1) % col].1.1 += 1;
-                queue.push_back((r, (col + c-1) % col));
-            }
-            if garden[r][(col + c+1) % col] != (('#', 0), ('#', 0)) && !queue.contains(&(r, (col + c+1) % col)) {
-                garden[r][(col + c+1) % col].1.0 = 'O';
-                garden[r][(col + c+1) % col].1.1 += 1;
-                queue.push_back((r, (col + c+1) % col));
+            
+            for (nr, nc) in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)] {
+                if garden[nr.rem_euclid(size as i128) as usize][nc.rem_euclid(size as i128) as usize] == '#' || visited.contains(&(nr, nc)) {
+                    continue;
+                }
+                queue.push_back((nr, nc, step - 1));
+                visited.insert((nr, nc));
             }
         }
-        for r in 0..row {
-            for c in 0..col {
-                let (prev, new) = garden[r][c];
-                garden[r][c] = (new, prev);
+        plots.len()
+    };
+
+    
+    // as you expand, assuming the rate in each direction stays the same, the number of visited plots will approach a 
+    // quadratic form.  You simply need to interpolate the quadratic function and then insert the desired steps
+    // The following finds where you start to converge (equal second differences) and then interpolates
+
+    let mut x = 0;
+    let mut vals = VecDeque::<usize>::new();
+    let mut fdiff: [usize; 3];
+    let mut sdiff: [usize; 2];
+
+    loop {
+        vals.push_back(reachable_plots(x));
+        x += 1;
+        println!("vals, x = {vals:?}, {x}");
+        if vals.len() >= 4 {
+            fdiff = [vals[1]-vals[0], vals[2]-vals[1], vals[3]-vals[2]];
+            sdiff = [fdiff[2]-fdiff[1], fdiff[1]-fdiff[0]];
+            if sdiff[1] == sdiff[0] {
+                break;
+            } else {
+                vals.pop_front();
             }
         }
     }
-    let res = garden.iter().flatten().filter_map(|((plot, count), _)| if plot == &'O' {Some(count)} else {None}).sum();
+
+    let offset = x - 4;
+
+    let alpha = vals[0] as i128;
+    let beta = vals[1] as i128;
+    let gamma = vals[2] as i128;
+
+    let c = alpha;
+    let a = (gamma - 2 * beta + c) / 2;
+    let b = beta - c - a;
+
+    let quad = |x: i128| {
+        (a * x.pow(2) + b * x + c) as usize
+    };
+
+    let res = quad(total_steps as i128 / (2 * size as i128) - offset as i128);
     println!("Part 2 = {}", res);
     res
 }
