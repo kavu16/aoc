f9f1f3b2323f2354f18c9b14c08d58da112384f1
Brady Edwards <brady.edwards16@gmail.com>
2024-06-13 23:52:37 -0400

    Day 23 Part 2

--- src/puzzles/day23.rs
+++ src/puzzles/day23.rs
@@ -1,5 +1,3 @@
-// use std::collections::{BinaryHeap, HashSet};
-
 #[test]
 
 fn test() {
@@ -31,7 +29,7 @@ fn test() {
 
     println!("Example 1:");
     assert_eq!(solve1(&example1), 94);
-    assert_eq!(solve2(&example1), 0);
+    assert_eq!(solve2(&example1), 154);
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -42,9 +40,9 @@ enum Direction {
     East, 
     West,
 }
+use std::collections::{HashMap, HashSet, VecDeque};
+
 use Direction::*;
-
-
 
 pub fn solve1(data: &String) -> usize {
     let mut grid = Vec::<Vec<char>>::new();
@@ -96,8 +94,110 @@ pub fn solve1(data: &String) -> usize {
     res
 }
 
-pub fn solve2(_data: &String) -> usize {
-    let res = 0;
+pub fn solve2(data: &String) -> usize {
+    let mut grid = Vec::<Vec<char>>::new();
+    for line in data.lines() {
+        grid.push(line.chars().collect());
+    }
+    let rows = grid.len() as i32;
+    let cols = grid[0].len() as i32;
+
+    let mut graph = HashMap::<(i32,i32), Vec<((i32,i32),i32)>>::new();
+    let start = (0,1);
+    let end = (rows-1, cols-2);
+    graph.insert(start, Vec::new());
+    graph.insert(end, Vec::new());
+
+    for r in 0..rows {
+        for c in 0..cols {
+            if grid[r as usize][c as usize] == '#' {continue;}
+
+            let mut paths = 0;
+            for [dr, dc] in [[0,1], [0,-1], [1,0], [-1,0]] {
+                if r + dr < 0 || r + dr == rows || 
+                    c + dc < 0 || c + dc == cols || 
+                    grid[(r+dr) as usize][(c+dc) as usize] == '#'
+                 {
+                    continue;
+                }
+                paths += 1;
+            }
+            if paths > 2 { graph.insert((r,c), Vec::new()); }
+        }
+    }
+
+    let mut queue = VecDeque::<((i32,i32),(i32,i32),i32)>::from(graph
+        .keys()
+        .map(|&(r, c)| ((r, c), (r, c), 0i32))
+        .collect::<Vec<((i32,i32),(i32, i32),i32)>>()
+    );
+
+    let mut seen = HashSet::<((i32,i32), (i32,i32))>::new();
+    while let Some((parent, curr, steps)) = queue.pop_front() {
+        if let Some(node) = graph.get_mut(&curr) {
+            if curr != parent {
+                node.push((parent, steps));
+                continue;
+            }
+        }
+        seen.insert((parent, curr));
+        let (r,c) = curr;
+        for [dr, dc] in [[0,1], [0,-1], [1,0], [-1,0]] {
+            if r + dr < 0 || r + dr == rows ||
+                c + dc < 0 || c + dc == cols ||
+                seen.contains(&(parent, (r+dr, c+dc))) ||
+                grid[(r+dr) as usize][(c+dc) as usize] == '#' {
+                    continue;
+                }
+            
+            queue.push_back((parent, (r+dr, c+dc), steps + 1));
+        }
+    }
+
+    let mut queue = VecDeque::<(i32,i32)>::new();
+    let mut seen = HashSet::<(i32,i32)>::new();
+    let mut border = Vec::<((i32,i32), (i32,i32))>::new();
+    queue.push_back(start);
+    while let Some(node) = queue.pop_front() {
+        if seen.contains(&node) {continue;}
+        seen.insert(node);
+        for (neighbor, _steps) in graph.get(&node).unwrap() {
+            if let Some(adj) = graph.get(neighbor) {
+                if adj.len() == 3 && !seen.contains(neighbor) {
+                    border.push((node, *neighbor));
+                    queue.push_back(*neighbor);
+                }
+            }
+        }
+    }
+
+    border.iter().for_each(|(parent, child)| {
+        if let Some(kill_idx) = graph.get(child).unwrap().iter().position(|&i| i.0 == *parent) {
+            graph.get_mut(child).unwrap().remove(kill_idx);
+        }
+    });
+
+    fn hike(curr_node: (i32, i32),
+            curr_steps: i32,
+            graph: &HashMap<(i32, i32), Vec<((i32,i32), i32)>>, 
+            visited: &mut HashSet<(i32,i32)>,
+            end: (i32,i32)
+        ) -> i32 {
+        if curr_node == end { return curr_steps; }
+        if visited.contains(&curr_node) { return 0; }
+
+        visited.insert(curr_node);
+        let mut max_hike = curr_steps;
+        for (node, steps) in graph.get(&curr_node).unwrap() {
+            max_hike = max_hike.max(hike(*node, curr_steps + steps, graph, visited, end));
+        }
+        visited.remove(&curr_node);
+
+        max_hike
+    }
+
+    let mut visited = HashSet::<(i32,i32)>::new();
+    let res = hike(start, 0, &graph, &mut visited, end) as usize;
     println!("Part 2 = {}", res);
     res
 }
